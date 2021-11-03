4bcf53c4af27cc18e7dcfb635959dc56e61156cc
Mike Harsch <mike@harschsystems.com>
2021-11-03 12:56:52 -0700

    add RELAY2plate

---
+++ RELAY2plate.js
@@ -0,0 +1,9 @@
+const BASEplate = require('./BASEplate');
+
+class RELAY2plate extends BASEplate {
+    constructor (addr) {
+        super(addr, "RELAY2");
+    }
+}
+
+module.exports = RELAY2plate;
--- plates.js
+++ plates.js
@@ -1,4 +1,5 @@
 var RELAYplate = require('./RELAYplate');
+var RELAY2plate = require('./RELAY2plate');
 var DAQCplate = require('./DAQCplate');
 var DAQC2plate = require('./DAQC2plate');
 var MOTORplate = require('./MOTORplate');
@@ -7,6 +8,7 @@ var TINKERplate = require('./TINKERplate');
 
 module.exports = {
     RELAYplate: RELAYplate,
+    RELAY2plate: RELAY2plate,
     DAQCplate: DAQCplate,
     DAQC2plate: DAQC2plate,
     MOTORplate: MOTORplate,
